8e863d34525deeafa81a944a131df9717b051757
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-03-10 15:43:38 +0100

    create data for entity resolver

---
+++ apps/entity-resolver/create-input-data.R
@@ -0,0 +1,52 @@
+# title: Create input data for entity resolver
+# desc: fetch relevant data from db, incl. additional information about entities
+# input: DB tbl matching_candidates_authors_books_posters and posters_authors
+# output: Rdata
+
+library(tidyverse)
+library(kabrutils)
+
+# get data
+con <- connect_db()
+candidates <- tbl(con, "matching_candidates_authors_books_posters") %>% 
+  collect()
+
+# Get id's in order to fetch addintional infos ----------------------------
+
+poster_author_ids <- candidates %>% 
+  filter(source_1 == "poster" | source_2 == "poster") %>% 
+  select(id_1, id_2) %>% 
+  pivot_longer(cols = c(id_1, id_2), names_to = "id_seq", values_to = "id") %>% 
+  distinct(id) %>% 
+  pull()
+
+book_author_ids <- candidates %>% 
+  filter(source_1 == "book" | source_2 == "book") %>% 
+  select(id_1, id_2) %>% 
+  pivot_longer(cols = c(id_1, id_2), names_to = "id_seq", values_to = "id") %>% 
+  distinct(id) %>% 
+  pull()
+
+
+# Get additional infos ----------------------------------------------------
+
+posters_wide <- tbl(con, "posters_wide") %>% 
+  distinct(author_id, author) %>% 
+  filter(author_id %in% poster_author_ids) %>% 
+  collect()
+
+books_wide <- tbl(con, "books_wide") %>% 
+  #distinct(author_id, author, book_title = name, isbn, subtitle, location, publisher, year) %>% 
+  filter(author_id %in% book_author_ids) %>% 
+  collect()
+
+DBI::dbDisconnect(con); rm(con)
+
+# Export data -------------------------------------------------------------
+
+export_data <- list(
+  "candidates" = candidates,
+  "poster_meta" = books_wide,
+  "poster_meta" = posters_wide)
+
+save(export_data, file = "apps/entity-resolver/data/candidates-authors.Rdata")
