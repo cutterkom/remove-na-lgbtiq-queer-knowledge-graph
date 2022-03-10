8f8cdec9163bb8265c7b264b9a441e0d802cb85a
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-03-10 15:14:48 +0100

    add source as a column

--- data-gathering/analysis/bigrams.R
+++ data-gathering/analysis/bigrams.R
@@ -20,10 +20,12 @@ source("R/entity-cleaning.R")
 con <- connect_db()
 books_authors <- tbl(con, "books_authors") %>%
   collect() %>% 
-  mutate(author_id = paste0("books_", author_id))
+  mutate(source = "book",
+         id = paste(source, author_id, sep = "_"))
 posters_authors <- tbl(con, "posters_authors") %>%
   collect() %>% 
-  mutate(author_id = paste0("poster_", author_id))
+  mutate(source = "poster",
+         id = paste(source, author_id, sep = "_"))
 dbDisconnect(con); rm(con)
 
 authors <- bind_rows(books_authors, posters_authors)
@@ -35,7 +37,7 @@ authors <- bind_rows(books_authors, posters_authors)
 string_mappings <- get(file = "static/string-mapping.yml") 
 
 bigrams_authors <- authors %>% 
-  select(id = author_id, name = author) %>% 
+  select(id, name = author) %>% 
   clean_string(col = "name") %>% 
   filter(
     !str_detect(tolower(name), paste0(string_mappings$nonsense, collapse = "|")),
@@ -57,12 +59,10 @@ bigrams <- corp %>%
 
 # Calculate similarities -> create candidate pairs -------------------------
 
-bigrams_sims <- calc_similarity(bigrams, method = "cosine", min_sim = 0.65)
-
-bigrams_sims_with_names <- bigrams_sims %>% 
-  # add plain strings
-  left_join(bigrams_authors, by = c("id_1" = "id")) %>% 
-  left_join(bigrams_authors, by = c("id_2" = "id"), suffix = c("_1", "_2"))
+bigrams_sims <- calc_similarity(bigrams, method = "cosine", min_sim = 0.65) %>% 
+  left_join(authors %>% select(-author), by = c("id_1" = "id")) %>% 
+  left_join(authors %>% select(-author), by = c("id_2" = "id"), suffix = c("_1", "_2")) %>% 
+  select(id_1 = author_id_1, id_2 = author_id_2, source_1, source_2, value, rank)
 
 # Write in DB -------------------------------------------------------------
 
@@ -70,12 +70,14 @@ create_table <- "
 CREATE TABLE IF NOT EXISTS `matching_authors_books_posters` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  `id_1` varchar(50) COLLATE utf8mb3_german2_ci DEFAULT NULL,
-  `id_2` varchar(50) COLLATE utf8mb3_german2_ci DEFAULT NULL,
+  `source_1` varchar(50) COLLATE utf8mb3_german2_ci DEFAULT NULL,
+  `id_1` int(11) COLLATE utf8mb3_german2_ci DEFAULT NULL,
+  `source_2` varchar(50) COLLATE utf8mb3_german2_ci DEFAULT NULL,
+  `id_2` int(11) COLLATE utf8mb3_german2_ci DEFAULT NULL,
   `value` float DEFAULT NULL,
   `rank` bigint DEFAULT NULL,
   `decision` varchar(1501) COLLATE utf8mb3_german2_ci NOT NULL DEFAULT 'no_judgement',
-  PRIMARY KEY `id_1` (`id_1`,`id_2`)
+  PRIMARY KEY `keys` (`id_1`,`id_2`, `source_1`, `source_2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_german2_ci COMMENT='Table to store calculation of similarities between entities. Afterwards there needs to be a human decision made. Decisions are written in this database.';
 "
 
@@ -83,5 +85,3 @@ con <- connect_db()
 dbExecute(con, create_table)
 dbAppendTable(con, "matching_authors_books_posters", bigrams_sims)
 dbDisconnect(con); rm(con)
-
-
