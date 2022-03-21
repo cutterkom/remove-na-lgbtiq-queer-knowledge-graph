612145e520b14347488be733602b4e7e4ad03e30
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-03-21 14:52:19 +0100

    align page number with pdf and remove when text is only page number

--- data-gathering/from-pdf/themengeschichtspfad.R
+++ data-gathering/from-pdf/themengeschichtspfad.R
@@ -10,7 +10,7 @@ pdf_data_list <- setNames(pdf_data_list, c(1:length(pdf_data)))
 # page as column
 pdf_as_data <- bind_rows(pdf_data_list, .id = "page") %>% 
   mutate(
-    page = as.numeric(page),
+    page = as.numeric(page) - 2,
     
     # mark content: before and after can be ignored
     content = 
@@ -33,13 +33,13 @@ pdf_as_data <- pdf_as_data %>%
         height == 6 ~ "image_caption",
         height == 8 ~ "p",
       )
-  )
+  ) 
 
 pdf_as_data %>% count(height, format, sort = T)
 
 collapsed_text <- pdf_as_data %>% 
   group_by(page, format) %>% 
   summarise(text_string = glue::glue_collapse(text, sep = " ", last = ""), .groups = "drop") %>% 
-  mutate(text_string = str_replace(text_string, "\\s-\\s", ""))
-
-
+  mutate(text_string = str_replace(text_string, "\\s-\\s", "")) %>% 
+  # remove, when text_string is only page number
+  filter(nchar(text_string) != 2)
