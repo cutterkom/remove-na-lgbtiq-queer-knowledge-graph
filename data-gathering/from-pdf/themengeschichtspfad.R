5b44b47739ab4f2d31f1388cdb6e2419127ce567
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-03-21 16:19:53 +0100

    new format cats based on font_info

--- data-gathering/from-pdf/themengeschichtspfad.R
+++ data-gathering/from-pdf/themengeschichtspfad.R
@@ -16,31 +16,34 @@ pdf_as_data <- bind_rows(pdf_data_list, .id = "page") %>%
     # mark content: before and after can be ignored
     content = 
       case_when(
-        page >= 15 & page < 154 ~ 1,
+        page >= 15 & page < 150 ~ 1,
         TRUE ~ 0
       )
   ) %>% 
   group_by(page) %>% 
   mutate(order = row_number()) %>% 
+  ungroup() %>% 
   # keep only real content, remove table of content, sources etc
-  filter(content == 1) 
-
-
-pdf_as_data <- pdf_as_data %>% 
+  filter(content == 1) %>% 
   mutate(
     format = 
       case_when(
-        height == 10 ~ "heading",
-        height == 6 ~ "image_caption",
-        height == 8 ~ "p",
+        font_size == 12.0 ~ "heading",
+        font_size == 8.8 & font_name == "AAAAAB+Univers-Bold" ~ "location_header",
+        font_size == 7.0 & font_name == "AAAAAB+Univers-Bold" ~ "location_roof_line",
+        font_size == 7.0 & font_name == "AAAAAC+Univers" ~ "image_caption",
+        font_size == 8.8 & font_name == "AAAAAD+Univers-Light" ~ "p",
+        font_size == 8.8 & font_name == "AAAAAC+Univers" ~ "chapter_intro",
+        font_size == 7.0 & font_name == "AAAAAD+Univers-Light" ~ "source",
+        round(font_size, 2) == 7.16 & font_name == "AAAAAC+Univers" ~ "image_caption_direction",
+        str_detect(text, "^[0-9]{1,3}$") ~ "page",
+        TRUE ~ "none" # there's one element, but does not matter (word "dem")
       )
-  ) 
-
-pdf_as_data %>% count(height, format, sort = T)
+  )
 
 collapsed_text <- pdf_as_data %>% 
-  group_by(page, format) %>% 
+  group_by(page, format) %>% #filter(page == 18) %>% View
   summarise(text_string = glue::glue_collapse(text, sep = " ", last = ""), .groups = "drop") %>% 
   mutate(text_string = str_replace(text_string, "\\s-\\s", "")) %>% 
   # remove, when text_string is only page number
-  filter(nchar(text_string) != 2)
+  filter(nchar(text_string) <= 3)
