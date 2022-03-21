4bfa1547d4661eb00282cfbf7f68f626fd4715a4
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-03-21 14:05:35 +0100

    add working books-author linked data mapping

--- data-modelling/create-rdf.py
+++ data-modelling/create-rdf.py
@@ -13,14 +13,14 @@ kg = kglab.KnowledgeGraph(
 
 kg.describe_ns()
 
-kg.materialize('configs/posters-config.ini')
+kg.materialize('configs/books-authors.ini')
 #kg.materialize('configs/books-config.ini')
 
 kg_string = kg.save_rdf_text(format="ttl")
 print(kg_string)
 
-kg.save_rdf("output/posters-rdf-triples.ttl")
-kg.save_jsonld("output/posters-rdf-triples.jsonld")
+kg.save_rdf("output/books-authors-triples.ttl")
+kg.save_jsonld("output/books-authors-triples.jsonld")
 
 #kg.save_rdf("output/books-rdf-triples.ttl")
 #kg.save_jsonld("output/books-rdf-triples.jsonld")
