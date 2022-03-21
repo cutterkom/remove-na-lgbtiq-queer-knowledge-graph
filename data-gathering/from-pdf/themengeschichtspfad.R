96d81163ff4121c312cde7bd1a9df4e8eeebadf6
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-03-21 16:27:07 +0100

    adjust to new input pdf

--- data-gathering/from-pdf/themengeschichtspfad.R
+++ data-gathering/from-pdf/themengeschichtspfad.R
@@ -11,12 +11,12 @@ pdf_data_list <- setNames(pdf_data_list, c(1:length(pdf_data)))
 # page as column
 pdf_as_data <- bind_rows(pdf_data_list, .id = "page") %>% 
   mutate(
-    page = as.numeric(page) - 2,
+    page = as.numeric(page),
     
     # mark content: before and after can be ignored
     content = 
       case_when(
-        page >= 15 & page < 150 ~ 1,
+        page >= 14 & page < 150 ~ 1,
         TRUE ~ 0
       )
   ) %>% 
@@ -28,22 +28,26 @@ pdf_as_data <- bind_rows(pdf_data_list, .id = "page") %>%
   mutate(
     format = 
       case_when(
+        str_detect(text, "^[0-9]{1,3}$") ~ "page",
         font_size == 12.0 ~ "heading",
         font_size == 8.8 & font_name == "AAAAAB+Univers-Bold" ~ "location_header",
-        font_size == 7.0 & font_name == "AAAAAB+Univers-Bold" ~ "location_roof_line",
-        font_size == 7.0 & font_name == "AAAAAC+Univers" ~ "image_caption",
         font_size == 8.8 & font_name == "AAAAAD+Univers-Light" ~ "p",
         font_size == 8.8 & font_name == "AAAAAC+Univers" ~ "chapter_intro",
+        font_size == 7.0 & font_name == "AAAAAB+Univers-Bold" ~ "location_roof_line",
+        font_size == 7.0 & font_name == "AAAAAC+Univers" ~ "image_caption",
         font_size == 7.0 & font_name == "AAAAAD+Univers-Light" ~ "source",
         round(font_size, 2) == 7.16 & font_name == "AAAAAC+Univers" ~ "image_caption_direction",
-        str_detect(text, "^[0-9]{1,3}$") ~ "page",
         TRUE ~ "none" # there's one element, but does not matter (word "dem")
       )
   )
 
+pdf_as_data
+
 collapsed_text <- pdf_as_data %>% 
-  group_by(page, format) %>% #filter(page == 18) %>% View
+  group_by(page, format) %>%
   summarise(text_string = glue::glue_collapse(text, sep = " ", last = ""), .groups = "drop") %>% 
-  mutate(text_string = str_replace(text_string, "\\s-\\s", "")) %>% 
+  mutate(text_string = str_replace(text_string, "\\s-\\s|-\\s", "")) %>% 
   # remove, when text_string is only page number
-  filter(nchar(text_string) <= 3)
+  filter(format != "page")
+
+collapsed_text
