3649c8ba8b10a7663167d651cd21513565ad27fc
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-04-06 14:15:08 +0200

    add name compare

--- data-linking/link-books-with-lobid.R
+++ data-linking/link-books-with-lobid.R
@@ -8,6 +8,7 @@ library(kabrutils)
 library(DBI)
 library(httr)
 library(jsonlite)
+library(fuzzyjoin)
 
 source("data-linking/lobid-functions.R")
 
@@ -15,7 +16,7 @@ source("data-linking/lobid-functions.R")
 # config ------------------------------------------------------------------
 
 # show print statements for debugging?
-verbose <- T
+verbose <- F
 
 # Get books data ----------------------------------------------------------
 
@@ -28,7 +29,11 @@ books <- tbl(con, "books_wide") %>%
     book_id = as.character(book_id)
   )
 
-el_matches <- tbl(con, "el_matches") %>% collect()
+el_matches <- tbl(con, "el_matches") %>%
+  filter(entity_id_type == "books") %>%
+  collect()
+
+# fetched_books <- el_matches %>% filter(entity_id_type == "books")
 
 DBI::dbDisconnect(con)
 rm(con)
@@ -43,15 +48,15 @@ get_lobid_ressource_id <- ".member[] | {source_id: .id}"
 books %>%
   rowid_to_column() %>%
   filter(!is.na(isbn)) %>%
-  distinct(rowid, book_id, isbn) %>% 
+  distinct(rowid, book_id, isbn) %>%
   # remove those that were already matched
-  #anti_join(el_matches, by = c("book_id" = "entity_id_combination", "id" = "entity_id")) %>%
-  #filter(rowid==2050) %>%
-  sample_n(1) %>% 
+  anti_join(el_matches, by = c("book_id" = "entity_id")) %>%
+  # filter(rowid==2050) %>%
+  # sample_n(1) %>%
   pmap_dfr(function(...) {
     current <- tibble(...)
-    
-    current_meta <- current %>% inner_join(books)
+
+    current_meta <- current %>% inner_join(books, by = c("book_id", "isbn"))
 
     cli::cli_alert("id: {current$rowid}")
 
@@ -67,90 +72,98 @@ books %>%
     # Get lobid author/contributor info ---------------------------------------
 
     if (!is.na(current$isbn)) {
-      contribution_agent <- res %>%
-        get_field_values(input_type = "response", get_agents) %>%
-        purrr::map(jsonlite::fromJSON) %>%
-        enframe() %>%
-        unnest_wider(value)
 
-      data_agent <- tibble(
-        entity_id = current_meta$id,
-        entity_id_combination = current$book_id,
-        entity_id_combination_type = "book",
-        entity_id_type = "entities"
-      ) 
-      
-      
       # compare person names with entities and keep them ------------------------
       if (check_items > 0) {
-      contribution_agent_names <- contribution_agent %>% 
-        select(-name) %>% 
-        distinct() %>% 
-        mutate(label2 = label) %>% 
-        separate(label2, ",", into = c("lastname", "rest_of_name")) %>%
-        mutate(
-          rest_of_name = trimws(rest_of_name),
-          name_compare = paste(rest_of_name, lastname)) %>% 
-        # compare names with fuzzy join
-        stringdist_inner_join(current_meta %>% select(entity_id = id, name), by = c( "name_compare" = "name"), max_dist = 2) #%>% 
-      
-      data_agent <- data_agent %>% 
-        left_join(contribution_agent_names %>% 
-                    select(-lastname, -rest_of_name, -name_compare))
+        contribution_agent <- res %>%
+          get_field_values(input_type = "response", get_agents) %>%
+          purrr::map(jsonlite::fromJSON) %>%
+          enframe() %>%
+          unnest_wider(value)
+
+        contribution_agent_names <- contribution_agent %>%
+          select(-name) %>%
+          distinct() %>%
+          mutate(label2 = label) %>%
+          separate(label2, ",", into = c("lastname", "rest_of_name")) %>%
+          mutate(
+            rest_of_name = trimws(rest_of_name),
+            name_compare = paste(rest_of_name, lastname)
+          ) %>%
+          # compare names with fuzzy join
+          stringdist_inner_join(current_meta %>% select(entity_id = id, name), by = c("name_compare" = "name"), max_dist = 2) # %>%
+
+        data_agent <- tibble(
+          entity_id = current_meta$id,
+          entity_id_combination = current$book_id,
+          entity_id_combination_type = "book",
+          entity_id_type = "entities"
+        ) %>%
+          left_join(contribution_agent_names %>%
+            select(-lastname, -rest_of_name, -name_compare))
       } else {
-        data_agent <-  data_agent %>%
-          bind_cols(contribution_agent) %>%
-          dplyr::select(-name) %>%
-          distinct()
+        data_agent <- tibble(
+          entity_id = current$id,
+          entity_id = current_meta$id,
+          entity_id_combination = current$book_id,
+          entity_id_combination_type = "book",
+          entity_id_type = "entities",
+          gnd_id = NA_character_,
+          type = NA_character_,
+          label = NA_character_,
+          role = NA_character_
+        )
       }
+    }
 
-      if (verbose == TRUE) {
-        cli::cli_h1("person")
-        print(data_agent)
-      }
 
-      # Get book topics ---------------------------------------------------------
+    if (verbose == TRUE) {
+      cli::cli_h1("person")
+      print(data_agent)
+    }
 
-      component_list <- res %>%
-        get_field_values(input_type = "response", get_components) %>%
+    # Get book topics ---------------------------------------------------------
+
+    component_list <- res %>%
+      get_field_values(input_type = "response", get_components) %>%
+      purrr::map(jsonlite::fromJSON) %>%
+      enframe() %>%
+      unnest_wider(value)
+
+    data_component <- tibble(
+      entity_id = current$book_id,
+      entity_id_combination = NA_character_,
+      entity_id_combination_type = NA_character_,
+      property_type = "topic",
+      entity_id_type = "books"
+    ) %>%
+      bind_cols(component_list) %>%
+      dplyr::select(-name) %>%
+      distinct()
+
+    if (verbose == TRUE) {
+      cli::cli_h1("topic")
+      print(data_component)
+    }
+
+
+    # Get ressource lobid  ----------------------------------------------------
+
+    if (check_items > 0) {
+      lobid_ressource_id <- res %>%
+        get_field_values(input_type = "response", get_lobid_ressource_id) %>%
         purrr::map(jsonlite::fromJSON) %>%
         enframe() %>%
-        unnest_wider(value)
-
-      data_component <- tibble(
-        entity_id = current$book_id,
-        entity_id_combination = NA_character_,
-        entity_id_combination_type = NA_character_,
-        property_type = "topic",
-        entity_id_type = "books"
-      ) %>%
-        bind_cols(component_list) %>%
-        dplyr::select(-name) %>%
+        unnest_wider(value) %>%
+        select(-name) %>%
+        mutate(source_id = glue::glue_collapse(source_id, sep = ", ")) %>%
         distinct()
 
       if (verbose == TRUE) {
-        cli::cli_h1("topic")
-        print(data_component)
+        cli::cli_h1("lobid_ressource_id")
+        print(lobid_ressource_id)
       }
 
-
-      # Get ressource lobid  ----------------------------------------------------
-
-      if (check_items > 0) {
-        lobid_ressource_id <- res %>%
-          get_field_values(input_type = "response", get_lobid_ressource_id) %>%
-          purrr::map(jsonlite::fromJSON) %>%
-          enframe() %>%
-          unnest_wider(value) %>%
-          select(-name) %>%
-          mutate(source_id = glue::glue_collapse(source_id, sep = ", ")) %>%
-          distinct()
-
-        if (verbose == TRUE) {
-          cli::cli_h1("lobid_ressource_id")
-          print(lobid_ressource_id)
-        }
-      }
 
 
 
@@ -234,6 +247,7 @@ books %>%
           external_id, external_id_type, external_id_desc, external_id_label,
           property_type, source, source_id
         )
+
       if (verbose == TRUE) {
         cli::cli_h1("import")
         print(import)
