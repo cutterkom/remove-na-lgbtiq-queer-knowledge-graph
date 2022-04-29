611ac5a65816e6959292dd5401f5b6a232ac36c2
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-04-29 09:27:06 +0200

    mv files

--- data-gathering/entity-resolution/chronik/02-distinct-entities-from-chronik.R
+++ data-gathering/entity-resolution/chronik/02-distinct-entities-from-chronik.R
@@ -10,18 +10,10 @@ con <- connect_db()
 
 entities_raw <- tbl(con, "chronik_entities") %>% 
   collect() %>% 
-  mutate(
-    name = 
-      case_when(
-        name == "Münchens" ~ "München",
-        name == "Augspurg" ~ "Anita Augspurg",
-        TRUE ~ name
-      )
-  )
+ rename(er_id = id)
 
-er_candiates_raw <- tbl(con, "er_candidates") %>% 
-  rename(cand_id = id) %>% 
-  collect()
+er_candiates_raw <- tbl(con, "er_candidates") %>% collect()
+  
 
 er_candidates <- er_candiates_raw %>% 
   filter(source_1 == "chronik", source_2 == "chronik") %>% 
@@ -36,33 +28,33 @@ DBI::dbDisconnect(con); rm(con)
 # Resolve manual decisions and create a new ID for those ------------------
 
 new_ids_raw <- er_candidates %>%
-  filter(decision == "positive") %>% 
+  filter(decision == "positive") %>%
   #head(1) %>% 
   pmap_dfr(function(...) {
     
-    current <- tibble(...)
+    current <<- tibble(...) #%>% 
+      # mutate(id_1 = paste0(source_1, "_", id_1),
+      #        id_2 = paste0(source_2, "_", id_2))
     
     if (verbose == TRUE) {
       message("current data:")
       print(current)
     }
     
-    candidate_1 <- entities_raw %>% 
-      inner_join(current, by = c("id" = "id_1")) %>% 
-      mutate(id_new = paste0("er_", cand_id)) %>% 
-      #select(id, id_new, name_1 = name, keep_label_2, new_label)
-      select(id, cand_id, name_1 = name, keep_label_2, new_label)
+    candidate_1 <<- entities_raw %>% 
+      inner_join(current, by = c("er_id" = "id_1")) %>% 
+      mutate(id_new = paste0("er_", er_id)) %>% 
+      select(id, id_new, name_1 = name, keep_label_2, new_label)
     
     if (verbose == TRUE) {
       message("candidate_1:")
       print(candidate_1)
     }
     
-    candidate_2 <- entities_raw %>% 
-      inner_join(current, by = c("id" = "id_2")) %>% 
-      #mutate(id_new = paste0("er_", cand_id)) %>% 
-      #select(id, id_new, name_2 = name, keep_label_2, new_label)
-      select(id, cand_id, name_2 = name, keep_label_2, new_label)
+    candidate_2 <<- entities_raw %>% 
+      inner_join(current, by = c("er_id" = "id_2")) %>% 
+      mutate(id_new = paste0("er_", er_id)) %>% 
+      select(id, id_new, name_2 = name, keep_label_2, new_label)
     
     if (verbose == TRUE) {
       message("candidate_2:")
@@ -70,20 +62,16 @@ new_ids_raw <- er_candidates %>%
     }
     
     er_pair <- candidate_1 %>% 
-      #left_join(candidate_2, by = c("id", "keep_label_2", "new_label"), suffix = c("_1", "_2")) %>% 
-      left_join(candidate_2, by = c("cand_id", "keep_label_2", "new_label"), suffix = c("_1", "_2")) %>% 
+      left_join(candidate_2, by = c("id", "keep_label_2", "new_label"), suffix = c("_1", "_2")) %>% 
       mutate(
         name = 
           case_when(
             keep_label_2 == 1 ~ name_2,
             !is.na(new_label) ~ new_label,
             TRUE ~ name_1
-          ),
-        id = paste0("er_", cand_id)
+          )
       ) %>% 
-      filter(!is.na(name)) %>% 
-      select(-cand_id) %>% 
-      select(id, everything())
+      filter(!is.na(name))
     
     if (verbose == TRUE) {
       message("er_pair:")
@@ -97,7 +85,7 @@ new_ids_raw <- er_candidates %>%
 
 # Save mapping of old to new ID -------------------------------------------
 
-id_mappings <- new_ids_raw %>% select(id_new = id, id_1, id_2)
+id_mappings <- new_ids_raw %>% select(id, id_1 = id_new_1, id_2 = id_new_2)
 
 
 # minor tweaks in new_ids df ----------------------------------------------
@@ -107,7 +95,6 @@ new_ids <- new_ids_raw %>%
   mutate(
     name = case_when(
       str_detect(name, "forum") ~ "Forum Queeres Archiv München e.V.",
-      name == "Münchens" ~ "München",
       TRUE ~ name)
   )
 
@@ -129,24 +116,21 @@ duplicate_new_ids <- new_ids %>%
   select(id_new, id_old = id, name)
 
 duplicate_new_ids_for_mapping <- duplicate_new_ids %>% 
-  filter(id_new != id_old) %>% 
-  select(id_new, er_old = id_old)
-  
+  filter(id_new != id_old)
 
-duplicate_new_ids_for_mapping
+
 # Add to ID mapping -------------------------------------------------------
 
-id_mappings_step_2 <- bind_rows(
+id_mappings <- bind_rows(
   id_mappings, 
   
   # map id (incl. "old" er_id's) and add to mappings df
   new_ids %>% 
     left_join(duplicate_new_ids_for_mapping, by = c("id" = "id_new")) %>%
-    select(id_new = id, er_old) %>% 
-    distinct() %>% 
-    filter(!is.na(id_new))
-) %>% 
-  filter(!is.na(id_1) | !is.na(id_2) | !is.na(er_old))
+    #rename(select(id_new = id, id_1, id_2, er_old = id_old)) %>% 
+  distinct() %>% 
+  filter(!is.na(id))
+)
 
 # add de-deduplicated IDs to df with all new entities ---------------------
 
@@ -156,25 +140,27 @@ new_ids_final <- bind_rows(
                           filter(n>1), by = "name"),
   duplicate_new_ids %>% select(id = id_new, name)
 ) %>% 
-  distinct()
+  distinct() %>% 
+  mutate(er_id = paste0("er_", id)) %>% 
+  select(-id)
 
 # Add to other entities ---------------------------------------------------
 
 # in order to create proper id's I first have to remove the new ER'ed id from entities df.
 
 remove_from_entities <- new_ids_final %>% 
-  mutate(er_id_to_join = as.numeric(str_remove(id, "er_"))) %>% 
-  left_join(er_candidates, by = c("er_id_to_join" = "cand_id")) %>% 
-  select(id, source_1, id_1, source_2, id_2)
+  mutate(er_id_to_join = as.numeric(str_remove(er_id, "er_"))) %>% 
+  left_join(er_candidates, by = c("er_id_to_join" = "id")) %>% 
+  select(er_id, source_1, id_1, source_2, id_2)
 
 remove_from_entities <- bind_rows(
-  remove_from_entities %>% select(source = source_1, id = id_1),
-  remove_from_entities %>% select(source = source_2, id = id_2)) 
+  remove_from_entities %>% select(source = source_1, er_id = id_1),
+  remove_from_entities %>% select(source = source_2, er_id = id_2)) 
 
 
 entities <- entities_raw %>% 
-  anti_join(remove_from_entities, by = c("id")) %>% 
-  select(id, name) %>% 
+  anti_join(remove_from_entities, by = c("er_id")) %>% 
+  select(er_id, name) %>% 
   bind_rows(new_ids_final) %>% 
   distinct()
 
@@ -191,11 +177,9 @@ duplicate_entities <- entities %>%
                filter(!name %in% c("Forum")), by = "name") %>%
   select(-n) %>%
   group_by(name) %>%
-  # arrange so that er_ is at first and will be kept
-  arrange(desc(id)) %>%  
-  mutate(rowid = row_number()) %>% 
+  mutate(rowid = row_number()) %>%
   ungroup() %>%
-  pivot_wider(id_col = name, names_from = rowid, values_from = id) %>% 
+  pivot_wider(id_col = name, names_from = rowid, values_from = er_id) %>%
   rename(id_old = `1`, id_new = `2`) %>% 
   mutate(
     id_new =
@@ -205,15 +189,12 @@ duplicate_entities <- entities %>%
         TRUE ~ id_new
       )
   ) %>%
-  select(name, id_new = id_old, id_old = id_new)
+  select(name, id_old, id_new)
 
 
 # Same removed ones in mapping df -----------------------------------------
 
-id_mappings_step_3 <- bind_rows(
-  id_mappings_step_2, 
-  duplicate_entities %>% select(id_new, id_1 = id_old)
-  )
+id_mappings <- bind_rows(id_mappings, duplicate_entities %>% select(id_new, id_1 = id_old))
 
 # Add de-duduplicated to entities df --------------------------------------
 
@@ -222,7 +203,7 @@ entities_final <- bind_rows(
   entities %>% anti_join(entities %>% 
                            count(name, sort = T) %>% 
                            filter(n>1), by = "name"),
-  duplicate_entities %>% select(id = id_new, name)
+  duplicate_entities %>% select(er_id = id_new, name)
 )
 
 
@@ -309,19 +290,23 @@ import <- entities_final %>%
   ) %>% 
   filter(!str_detect(label, "DATE")) %>% 
   distinct() %>% 
-  select(-id.y, -chronik_entry_id, -label) %>% 
-  rename(id =  id.x) %>% 
+  select(-er_id.y, -chronik_entry_id, -label) %>% 
+  rename(id =  er_id.x) %>% 
   mutate(
     # München
     location = 
       case_when(
         id == "chronik_63" ~ 0, 
         id == "chronik_672" ~ 0,
+        name == "Köln" ~ 0,
+        name == "Holzplatz" ~ 1,
         id == "chronik_662" ~ 1, 
+        name == "Frauenbibliothek" ~ 1,
         TRUE ~ location),
     city = case_when(
       id == "chronik_63" ~ 1, 
       id == "chronik_672" ~ 1,
+      name == "Köln" ~ 1,
       TRUE ~ city),
     
     club = case_when(
@@ -332,31 +317,48 @@ import <- entities_final %>%
       id == "chronik_662" ~ 1, 
       club == 1 ~ 1,
       name == "VSG" ~ 1,
+      name == "Frauenbibliothek" ~ 1,
       TRUE ~ org
     )
-  ) %>% 
-  distinct() %>% 
-  # can happen that one entity has multiple labels, no keep all 1 (max)
-  group_by(name, id) %>% 
-  summarise_all(max)
-
+  ) %>% distinct() %>% 
+  group_by(id) %>% 
+  dplyr::summarise_(max)
+  #arrange(org, club) %>% 
 testthat::test_that(
   desc = "all unique",
   expect_unique(c("id", "name"), data = import)
 )
 
-import %>% View()
+import %>% count(name, sort = T) %>% filter(n>1) %>% 
+  left_join(import) %>% View
 
 con <- connect_db(credential_name = "db_clean")
 DBI::dbAppendTable(con, "entities", import)
-DBI::dbDisconnect(con); rm(con)
+dbDisconnect(con); rm(con)
+
+
+
+
+
 
 
 # Save mappings table -----------------------------------------------------
 
-import <- id_mappings_step_3
+import <- id_mappings
+
+create_table <- "
+CREATE TABLE `id_mapping` (
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `id_new` varchar(50) DEFAULT NULL,
+  `id_1` varchar(50) DEFAULT NULL,
+  `id_2` varchar(50) DEFAULT NULL,
+  `er_old` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table that saves old IDs from manual mapping processes. Look up in lgbtiq_kg database.';
+"
 
 con <- connect_db(credential_name = "db_clean")
-DBI::dbAppendTable(con, "id_mapping", import)
-DBI::dbDisconnect(con); rm(con)
+dbExecute(con, create_table)
+dbAppendTable(con, "id_mapping", import)
+dbDisconnect(con); rm(con)
 
