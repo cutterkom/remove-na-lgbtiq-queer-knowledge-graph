58855cbac0d43286522572917479c6d0c078e093
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-06-15 11:15:25 +0200

    fetch wikidata revisions data and save to googlesheet

--- data-publishing/wikidata/get-api-data.R
+++ data-publishing/wikidata/get-api-data.R
@@ -2,34 +2,95 @@
 
 library(WikipediR)
 library(tidyverse)
+library(tidywikidatar)
+library(googlesheets4)
 
 domain <- "wikidata.org"
 project <- "wikidata"
 user <- c("Removena")
-limit <- 5
+limit <- 500
+
+# data will be saved in googlesheets
+gs_file <- "1zQ2CO6fkKUg4RFN8TACOxa2ZnSg2F45saBwDNphlzGY"
 
 # Get all contributions ---------------------------------------------------
 
-get_inital_batch <- function() {
-  data <- user_contributions(domain = domain, project = project, 
+mw_api_get_inital_batch <- function(limit = 500) {
+  data <- WikipediR::user_contributions(domain = domain, project = project, 
                              username = user, limit = limit)
 }
 
-get_next_batch <- function(previous_data = NULL) {
+mw_api_get_next_batch <- function(starting_point = NULL, limit = 500, ...) {
   # do more data exist?
-  if(!is.null(previous_data[["continue"]][["uccontinue"]])) {
-    next_data <- user_contributions(domain = domain, project = project, 
+  if(!is.null(starting_point)) {
+    next_data <- WikipediR::user_contributions(domain = domain, project = project, 
                                     username = user, limit = limit, 
-                                    continue = previous_data[["continue"]][["uccontinue"]])
+                                    continue = starting_point)
   }
 }
 
-inital_data <- get_inital_batch()
-next_batch <- get_next_batch(inital_data)
-
-next_batch2 <- get_next_batch(next_batch)
-
+mw_api_get_more_results_starting_point <- function(data) {
+  data[["continue"]][["uccontinue"]] 
+}
 
 
-#inital_data[["query"]][["usercontribs"]] %>% map_df(., ~print(.))
-#revisions_on_data <- revisions_df %>% filter(str_detect(tags, "wikidata-ui|OAuth"))
+# Start fetching data -----------------------------------------------------
+
+inital_data <- mw_api_get_inital_batch()
+check_for_more <- mw_api_get_more_results_starting_point(inital_data)
+
+# save all data in a list
+all_data <- inital_data[["query"]][["usercontribs"]]
+
+while(!is.null(check_for_more)){
+  # get current scroll status
+  current_data <- mw_api_get_next_batch(starting_point = check_for_more)
+  #append to data stack
+  all_data <- append(all_data, current_data[["query"]][["usercontribs"]])
+  #update scroll 
+  check_for_more <- mw_api_get_more_results_starting_point(current_data)
+}
+
+# fetch only relevant data
+# aka: real revisions on data and not wiki talk stuff etc
+# wikidata-ui: change on website
+# OAuth: change with Quickstatements or API
+revisions <- map_df(all_data, ~print(.)) %>% 
+  filter(str_detect(tags, "wikidata-ui|OAuth")) %>% 
+  # extract date
+  mutate(
+    timedate = lubridate::as_datetime(timestamp),
+    date = lubridate::date(timedate),
+    # get wikidata labels of changed items
+    label_en = tw_get_label(id = title, language = "en"),
+    label_de = tw_get_label(id = title, language = "de")
+  )
+
+
+# write to sheet
+write_sheet(revisions, ss = gs_file, sheet = "raw_data")
+
+# # test
+# testdata <- user_contributions(domain = domain, project = project, 
+#                                username = user, limit = 500)
+# 
+# testdata_df <- purrr::map_df(testdata[["query"]][["usercontribs"]], ~print(.))
+# 
+# testdata_df %>% count(revid, sort = T)
+# testdata_df %>% distinct()
+# 
+# testdata[["query"]][["usercontribs"]] %>% enframe() %>% unnest_longer("value")
+# all_equal(testdata_df, revisions)
+
+
+# count_per_date <- revisions %>% 
+#   count(date, sort = T)
+# 
+# count_per_date %>% 
+#   ggplot(aes(x = date, y = n)) +
+#   geom_col() +
+#   theme_minimal() +
+#   labs(title = "Anzahl der Veränderungen in Wikidata",
+#        subtitle = "Account: Removena", 
+#        x = "Anzahl",
+#        y = "Datum" )
