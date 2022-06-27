e5896a9d170b6aa1dea812f192aaa84c757c827c
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-06-27 12:09:00 +0200

    update packges to install

--- apps/compare-factgrid-wikidata/install_packages.R
+++ apps/compare-factgrid-wikidata/install_packages.R
@@ -1,3 +1,11 @@
-packages <- c("glue", "SPARQL", "here", "flexdashboard", "clipr", "shinyFeedback", "devtools")
+packages <- c("tidyverse", 
+              "shiny", 
+              "glue", 
+              "here", 
+              "flexdashboard", 
+              "shinyFeedback", 
+              "devtools")
 install.packages(packages)
 devtools::install_github("cutterkom/kabrutils")
+devtools::install_github("cutterkom/SPARQL")
+devtools::install_github("TS404/WikidataR")
