51b7dccc8ea3aa5a12c5842671418fadc0180573
Katharina Brunner <cutterkom@users.noreply.github.com>
2022-08-25 10:32:54 +0200

    Update df_filter_helper.R

--- apps/compare-factgrid-wikidata/df_filter_helper.R
+++ apps/compare-factgrid-wikidata/df_filter_helper.R
@@ -33,12 +33,12 @@ input_properties_vec <- "P304"
 # input_item_filter_value <- ""
 
 
-input_properties_vec <- "P702_P144"
+input_properties_vec <- "P165_P106"
+input_item_filter_value <- ""
 all_properties <- get_properties()
 input_properties <- get_input_properties(all_properties, input_properties_vec)
 comparison_raw <- get_comparison(input_properties_vec, input_item_filter_property, input_item_filter_value)
 
-comparison_raw %>% distinct(meta_property_id)
 
 in_both <- comparison_raw %>% 
   filter(is_same == "true") %>% 
@@ -54,18 +54,18 @@ only_in_wd <- comparison_raw %>%
   # remove those that are present in wd
   anti_join(in_both, by = c("fg_item", "wd_value_from_wd")) %>%
   # add property info
-  left_join(input_properties %>% select(fg_property_id, fg_propertyLabel, fg_property, wd_property, wd_property_id, meta_property_id), by = c("meta_property_id"))
-
-only_in_wd
+  left_join(input_properties %>% select(fg_property_id, fg_propertyLabel, fg_property, wd_property, wd_property_id, meta_property_id), by = c("meta_property_id", "fg_property_id", "wd_property_id"))
 
 
 only_in_fg <- comparison_raw %>% 
-  filter(is.na(is_same), !is.na(fg_item)) %>% 
+  filter(is_same != "true" | is.na(is_same), !is.na(fg_item)) %>% 
   distinct(fg_item, fg_itemLabel, wd_item, fg_property_id, fg_value, fg_valueLabel, wd_value_from_fg, wd_value_from_wd, meta_property_id) %>% 
-  # remove those that are present in wd
   anti_join(in_both, by = c("fg_item", "wd_value_from_fg")) %>% 
+  anti_join(only_in_wd, by = c("fg_item", "fg_itemLabel", "wd_item", "wd_value_from_wd", "fg_property_id", "meta_property_id")) %>% 
+  # add property info
   # add property info
   left_join(input_properties %>% select(fg_property_id, fg_propertyLabel, fg_property, wd_property, meta_property_id), by =  c("meta_property_id", "fg_property_id"))
 
 only_in_fg %>% glimpse()
 
+only_in_fg
